621a2d34a31724e7c194f365517db78e57183af4
lommen1 <daniel.widfeldt.lomas@brave.se>
2024-05-16 21:39:38 +0200

    Initial Commit

---
+++ RepCounter/RepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepCounterApp.swift
+//  RepCounter
+//
+//  Created by Daniel Widfeldt Lomås on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct RepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
